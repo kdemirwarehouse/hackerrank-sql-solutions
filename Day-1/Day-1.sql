d9817ab527525c166c13afeff7fdc9698404a7f7
Kadir Demir <demirabdulkadir04@gmail.com>
2026-03-13 11:07:29 +0300

    Day-1 sql solutions added

---
+++ Day-1/Day-1.sql
@@ -0,0 +1,17 @@
+"""
+HackerRank SQL Solutions - Day 1
+Date: 2025-03-13
+Author: Kadir Demir
+"""
+
+
+# ---------------------------------------------------------
+# PROBLEM 1: Revising the Select Query I
+# ---------------------------------------------------------
+
+
+SELECT * 
+FROM CITY
+WHERE COUNTRYCODE = 'USA'
+AND POPULATION > 100000;
+
