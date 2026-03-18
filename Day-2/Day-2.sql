212ee414ce2cee1977a65b54208afc316cdeae4c
Kadir Demir <demirabdulkadir04@gmail.com>
2026-03-18 10:12:14 +0300

    Day-2 solutions added

---
+++ Day-2/Day-2.sql
@@ -0,0 +1,12 @@
+"""
+HackerRank SQL Solutions - Day 2
+Date: 2025-03-18
+Author: Kadir Demir
+"""
+
+# ---------------------------------------------------------
+# PROBLEM 1: Japanese Cities' Names
+# ---------------------------------------------------------
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
