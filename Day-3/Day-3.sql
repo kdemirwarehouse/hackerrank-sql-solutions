5d35fff2f7e479525b959ab21ac153f08ae2353e
Kadir Demir <demirabdulkadir04@gmail.com>
2026-03-30 12:46:29 +0300

    Add query for Weather Observation Station 7

--- Day-3/Day-3.sql
+++ Day-3/Day-3.sql
@@ -15,3 +15,12 @@ OR CITY LIKE 'I%' OR
 CITY LIKE 'O%' OR CITY
 LIKE 'U%'
 
+# ---------------------------------------------------------
+# PROBLEM 2: Weather Observation Station 7
+# ---------------------------------------------------------
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY LIKE '%a' OR
+CITY LIKE '%e' OR CITY LIKE '%i'
+OR CITY LIKE '%o' OR CITY LIKE '%u';
+
