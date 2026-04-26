fb447b3eb1ff4d4363354058df0b41baee4a4215
Kadir Demir <demirabdulkadir04@gmail.com>
2026-04-26 20:01:22 +0300

    Add SQL query for Weather Observation Station 11

--- Day-3/Day-3.sql
+++ Day-3/Day-3.sql
@@ -63,6 +63,21 @@ CITY NOT LIKE '%i' AND
 CITY NOT LIKE '%o' AND
 CITY NOT LIKE '%u';
 
-
-
+# ---------------------------------------------------------
+# PROBLEM 5: Weather Observation Station 11
+# ---------------------------------------------------------
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY
+NOT LIKE 'A%' AND
+CITY NOT LIKE 'E%' AND
+CITY NOT LIKE 'I%' AND
+CITY NOT LIKE 'O%' AND
+CITY NOT LIKE 'U%'
+OR
+CITY NOT LIKE '%a' AND
+CITY NOT LIKE '%e' AND
+CITY NOT LIKE '%i' AND
+CITY NOT LIKE '%o' AND
+CITY NOT LIKE '%u';
 
