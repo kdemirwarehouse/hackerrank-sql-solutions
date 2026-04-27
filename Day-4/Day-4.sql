25902092eddb56f611ef2fc511a4f796032b4e2f
Kadir Demir <demirabdulkadir04@gmail.com>
2026-04-27 23:51:11 +0300

    Add query for employees with salaries over 2000

--- Day-4/Day-4.sql
+++ Day-4/Day-4.sql
@@ -28,3 +28,11 @@ SELECT name
 FROM Employee
 ORDER BY name ASC;
 
+# ---------------------------------------------------------
+# PROBLEM 3: Employee Salaries
+# ---------------------------------------------------------
+SELECT NAME
+FROM EMPLOYEE
+WHERE SALARY > 2000 AND
+MONTHS < 10
+ORDER BY EMPLOYEE_ID ASC;
