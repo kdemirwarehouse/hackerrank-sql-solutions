3a372b59a6f2ce31e6f3cd6c30e4ef9353cd3a4a
Kadir Demir <demirabdulkadir04@gmail.com>
2026-06-07 11:47:40 +0300

    Add SQL query for Weather Observation Station 16

--- Day-8/Day-8.sql
+++ Day-8/Day-8.sql
@@ -12,4 +12,13 @@ SELECT ROUND(LONG_W, 4)
 FROM STATION
 WHERE LAT_N = (SELECT MAX(LAT_N) FROM STATION WHERE LAT_N < 137.2345)
 
+# ---------------------------------------------------------
+# PROBLEM 2: Weather Observation Station 16
+# ---------------------------------------------------------
+SELECT ROUND(LAT_N, 4)
+FROM STATION
+WHERE LAT_N = (SELECT MIN(LAT_N)
+FROM STATION
+WHERE LAT_N > 38.7780)
+
 
