2e8b16b8fa0228385538d1a644fe0325fc5e626e
Kadir Demir <demirabdulkadir04@gmail.com>
2026-06-07 11:43:55 +0300

    Day-8 solutions added

---
+++ Day-8/Day-8.sql
@@ -0,0 +1,15 @@
+"""
+HackerRank SQL Solutions - Day 8
+Date: 2025-06-07
+Author: Kadir Demir
+"""
+
+
+# ---------------------------------------------------------
+# PROBLEM 1: Weather Observation Station 15
+# ---------------------------------------------------------
+SELECT ROUND(LONG_W, 4)
+FROM STATION
+WHERE LAT_N = (SELECT MAX(LAT_N) FROM STATION WHERE LAT_N < 137.2345)
+
+
