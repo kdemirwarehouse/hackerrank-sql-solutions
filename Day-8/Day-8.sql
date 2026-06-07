3886ba7c2a2d5c2393fd2c68189dd4d15214571d
Kadir Demir <demirabdulkadir04@gmail.com>
2026-06-07 12:59:57 +0300

    Day-8 solutions added

--- Day-8/Day-8.sql
+++ Day-8/Day-8.sql
@@ -13,3 +13,34 @@ FROM STATION
 WHERE LAT_N = (SELECT MAX(LAT_N) FROM STATION WHERE LAT_N < 137.2345)
 
 
+# ---------------------------------------------------------
+# PROBLEM 2: Weather Observation Station 16
+# ---------------------------------------------------------
+SELECT ROUND(LAT_N, 4)
+FROM STATION
+WHERE LAT_N = (SELECT MIN(LAT_N)
+FROM STATION
+WHERE LAT_N > 38.7780)
+
+# ---------------------------------------------------------
+# PROBLEM 3: Weather Observation Station 17
+# ---------------------------------------------------------
+SELECT ROUND(LONG_W, 4)
+FROM STATION
+WHERE LAT_N = (SELECT MIN(LAT_N)
+FROM STATION
+WHERE LAT_N > 38.7780)
+
+# ---------------------------------------------------------
+# PROBLEM 4: Weather Observation Station 18
+# ---------------------------------------------------------
+SELECT ROUND(ABS(MIN(LAT_N) - MAX(LAT_N)) + ABS(MIN(LONG_W) - MAX(LONG_W)), 4)
+FROM STATION;
+
+
+# ---------------------------------------------------------
+# PROBLEM 5: Weather Observation Station 19
+# ---------------------------------------------------------
+SELECT ROUND(SQRT(POWER(MIN(LAT_N) - MAX(LAT_N), 2) +
+POWER(MIN(LONG_W) - MAX(LONG_W), 2)), 4)
+FROM STATION;
