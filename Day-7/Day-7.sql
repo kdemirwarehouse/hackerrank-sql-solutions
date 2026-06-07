5aee10dc9482d59f8f5abc69279e5d873d74d65b
Kadir Demir <demirabdulkadir04@gmail.com>
2026-06-07 11:04:29 +0300

    Add SQL query for new companies and their managers

--- Day-7/Day-7.sql
+++ Day-7/Day-7.sql
@@ -13,3 +13,23 @@ WHERE MONTHS * SALARY = (
     SELECT MAX(MONTHS*SALARY)
     FROM EMPLOYEE
 );
+
+
+# ---------------------------------------------------------
+# PROBLEM 2: New Companies
+# ---------------------------------------------------------
+
+SELECT
+    c.company_code,
+    c.founder,
+    COUNT(DISTINCT lm.lead_manager_code) AS total_lead_managers,
+    COUNT(DISTINCT sm.senior_manager_code) AS total_senior_managers,
+    COUNT(DISTINCT m.manager_code) AS total_managers,
+    COUNT(DISTINCT e.employee_code) AS total_employees
+FROM Company c
+JOIN Lead_Manager   lm ON c.company_code  = lm.company_code
+JOIN Senior_Manager sm ON lm.company_code = sm.company_code
+JOIN Manager        m  ON sm.company_code = m.company_code
+JOIN Employee       e  ON m.company_code  = e.company_code
+GROUP BY c.company_code, c.founder
+ORDER BY c.company_code;
