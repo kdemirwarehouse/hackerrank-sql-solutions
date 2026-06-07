e76bcd74397e5c9dae3e404582718dbf1ad18bc5
Kadir Demir <demirabdulkadir04@gmail.com>
2026-06-07 12:57:33 +0300

    Day-9 solutions added

---
+++ Day-9/Day-9.sql
@@ -0,0 +1,15 @@
+"""
+HackerRank SQL Solutions - Day 9
+Date: 2025-06-07
+Author: Kadir Demir
+"""
+
+# ---------------------------------------------------------
+# PROBLEM 1: Weather Observation Station 20
+# ---------------------------------------------------------
+SELECT ROUND(LAT_N, 4)
+FROM (
+    SELECT LAT_N,ROW_NUMBER() OVER (ORDER BY LAT_N) AS rn
+    FROM STATION
+) AS ranked
+WHERE rn = (SELECT CEIL(COUNT(*) / 2) FROM STATION)
